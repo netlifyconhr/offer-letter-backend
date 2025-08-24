4ba5e5e6d7677ffc47a83e9322c008ef2b4ae0f5
netlifyconhr <netlifyconhr@gmail.com>
2025-08-24 16:19:21 +0530

    changes

--- dist/app/modules/customer/customer.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const customer_controller_1 = require("./customer.controller");
-const router = (0, express_1.Router)();
-// Define routes
-router.get('/', customer_controller_1.customerController.getAll);
-exports.default = router;
--- dist/app/modules/user/user.constant.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserSearchableFields = void 0;
-exports.UserSearchableFields = [
-    'email',
-    'name',
-    'role'
-];
