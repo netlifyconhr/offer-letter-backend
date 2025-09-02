0c57536e5ed7c18d505234b3824f72b41eaa12b9
netlifyconhr <netlifyconhr@gmail.com>
2025-09-02 22:33:51 +0530

    test

---
+++ src/app/modules/candidate-exam/candidate-exam.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import { candidateExamController } from "./candidate-exam.controller";
+import auth from "../../middleware/auth";
+import { multerUpload } from "../../config/multer.config";
+import { UserRole } from "../user/user.interface";
+import { parseBody } from "../../middleware/bodyParser";
+
+const router = Router();
+
+router.get("/", candidateExamController.getcandidateExamAll);
+router.get(
+  "/:id",
+  auth(UserRole.ADMIN, UserRole.USER),
+  candidateExamController.getcandidateExamById
+);
+
+router.post(
+  "/",
+  multerUpload.single("cv"),
+  parseBody,
+  candidateExamController.createcandidateExam
+);
+
+router.patch("/:candidateId", candidateExamController.updateCandidateExamById);
+export const CandidateExamRoutes = router;
